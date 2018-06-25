0325f57abfc3e435da8e8385cf1a1cfd089750d6
Matthew Stromberg <mstromb1@binghamton.edu>
2018-06-25 09:14:37 -0400

    bLANK

--- app3.js
+++ app3.js
@@ -2,7 +2,9 @@ const express = require('express');
 const fs = require('fs');
 
 var app = express();
-//var fs = fs();
+
+app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
 
 app.listen(3000); //Listening to port 3000
 app.get("/test/", function(req, res) {
@@ -12,9 +14,18 @@ app.get("/test/", function(req, res) {
 app.get('/:name', function(req, res) {
   var path = req.params.name;
   if (!fs.existsSync(path)) {
-    res.send("DOES NOT EXIST");
+    console.log("Profile is rendered");
+    res.render('profile', {
+      name: req.params.name
+    });
   } else {
     res.sendFile(__dirname + "/" + req.params.name);
 
   }
+
 });
+
+/*app.use(function(req, res) {
+  res.send("Not found!");
+});
+*/
